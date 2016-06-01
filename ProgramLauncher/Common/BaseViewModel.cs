b0115771f791bde2dfab8f1108990be15f511cbe
Daniel Reed <danielre44@gmail.com>
2016-06-01 06:42:37 -0400

    Updated

---
+++ ProgramLauncher/Common/BaseViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramLauncher.Common
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Protected Abstract Methods
+
+        protected abstract void HandleInternalPropertyChanged(string property);
+
+        #endregion
+
+        #region Protected Methods
+
+        protected void SetProperty<T>(T newValue, ref T currentValue, [CallerMemberName] string callerName = "")
+            where T : class
+        {
+            if (false == currentValue.Equals(newValue))
+            {
+                currentValue = newValue;
+                this.OnPropertyChanged(callerName);
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void OnPropertyChanged(string property)
+        {
+            this.PropertyChanged(this, new PropertyChangedEventArgs(property));
+            this.HandleInternalPropertyChanged(property);
+        }
+
+        #endregion
+    }
+}
--- ProgramLauncher/View/BasicFileViewModel.cs
+++ ProgramLauncher/View/BasicFileViewModel.cs
@@ -1,14 +1,17 @@
-﻿using ProgramLauncher.Model;
+﻿using ProgramLauncher.Common;
+using ProgramLauncher.Model;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace ProgramLauncher.View
 {
-    public class BasicFileViewModel
+    public class BasicFileViewModel : BaseViewModel
     {
 
         #region Fields
@@ -16,6 +19,11 @@ namespace ProgramLauncher.View
         private readonly ObservableCollection<FileData> _fileDataList;
         private readonly ReadOnlyObservableCollection<FileData> _readOnlyFileDataList;
 
+        private readonly ObservableCollection<FileData> _filteredFileDataList;
+        private readonly ReadOnlyObservableCollection<FileData> _readOnlyfilteredFileDataList;
+
+        private string _inputText;
+        
         #endregion
 
         #region Constructors
@@ -25,8 +33,14 @@ namespace ProgramLauncher.View
             this._fileDataList = new ObservableCollection<FileData>();
             this._readOnlyFileDataList = new ReadOnlyObservableCollection<FileData>(this._fileDataList);
 
+            this._filteredFileDataList = new ObservableCollection<FileData>(this._fileDataList.ToList());
+            this._readOnlyfilteredFileDataList = new ReadOnlyObservableCollection<FileData>(this._filteredFileDataList);
+
+            this._inputText = string.Empty;
+
             fileModel.FileAdded += this.FileAddedHandler;
             fileModel.FileRemoved += this.FileRemovedHandler;
+            
         }
 
         #endregion
@@ -41,10 +55,66 @@ namespace ProgramLauncher.View
             }
         }
 
+        public ReadOnlyObservableCollection<FileData> FilteredFileDataList
+        {
+            get
+            {
+                return this._readOnlyfilteredFileDataList;
+            }
+        }
+
+        public string InputText
+        {
+            get
+            {
+                return _inputText;
+            }
+
+            set
+            {
+                this.SetProperty(value, ref this._inputText);
+            }
+        }
+
+
+        #endregion
+
+        #region Protected BaseViewModel Methods
+
+        protected override void HandleInternalPropertyChanged(string property)
+        {
+            Logger.LogTrace("Property changed: " + property);
+
+            if (property.Equals(nameof(this.InputText)))
+            {
+                this.ApplyFilter();
+            }
+            else if (property.Equals("Test"))
+            {
+                this.ApplyFilter();
+            }
+
+
+            //if (property.Equals("InputText"))
+        }
+
         #endregion
 
         #region Private Methods
 
+        private void ApplyFilter()
+        {
+            this._filteredFileDataList.Clear();
+            
+            if (string.IsNullOrEmpty(this.InputText))
+            {
+                this._fileDataList.FirstOrDefault(x => { this._filteredFileDataList.Add(x); return false; });
+            }
+            else
+            { 
+                this._fileDataList.Where(x => x.FileName.Contains(this.InputText)).FirstOrDefault(x => { this._filteredFileDataList.Add(x); return false; });
+            }
+        }
 
         /*
          * TODO: We should be on the GUI thread.. verify?
@@ -52,12 +122,18 @@ namespace ProgramLauncher.View
 
         private void FileAddedHandler(FileData fileData)
         {
-            _fileDataList.Add(fileData);
+            this._fileDataList.Add(fileData);
+            this._filteredFileDataList.Add(fileData);
+
+            this.HandleInternalPropertyChanged("Test");
         }
 
         private void FileRemovedHandler(FileData fileData)
         {
-            _fileDataList.Remove(fileData);
+            this._fileDataList.Remove(fileData);
+            this._filteredFileDataList.Remove(fileData);
+            
+            this.HandleInternalPropertyChanged("Test");
         }
 
         #endregion
